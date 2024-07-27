6066ac4921de02350965bc390cafa381ffd81306
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-27 19:19:52 -0300

    Product entity

---
+++ src/main/java/andersonfflores/ecommerce/Models/Product/Product.java
@@ -0,0 +1,69 @@
+package andersonfflores.ecommerce.Models.Product;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Table("product")
+public class Product {
+
+    @Id
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(UUID id, String name, String description, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Product(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
